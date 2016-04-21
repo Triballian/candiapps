c71e7d12c8034e997338a01754908f619f62f23a
Triballian <candiapps@gmail.com>
2016-04-20 21:53:00 -0500

    cleaned up path input for simplicity

--- utils.py
+++ utils.py
@@ -1,3 +1,31 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2016 Noe De La Cruz, Jr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Created on Apr 16, 2016
+@author: Noe De La Cruz
+
+
+'''
 #utils.py python3 
 from re import sub
 
@@ -10,11 +38,16 @@ def getconf(name):
    
 
     for line in cfile:
-        #print('this is the line: ' + line)
+        
 
         marker = '#'
 
-        crline = line.partition(marker)[0].strip()
+        
+
+        crline = line.partition(marker)[0].strip().replace('\\', '/')
+        crline = sub(r'[C|c]:|/$', '', crline)
+        
+        print('this is the line: crline ' + crline)
         
 
 
@@ -35,6 +68,7 @@ def getconf(name):
         #print('this is the line: cdata[1] ' + str(cdata[1]))
         print('this is the line:' + str(line))
         envars[str(cdata[0].lower())] = sub('\[|\]|\'', '' , str(cdata[1].lstrip().split(',')))
+        
         
 
 
